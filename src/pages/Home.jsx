4b119e37bc457386956193a00f46acbc2d5055a4
Jake Littleton <jakelittleton30@gmail.com>
2026-07-03 10:12:55 -0500

    Homepage adjusted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <main className='text-slate-900 pl-10 pr-10'>
-      <h1 className='text-4xl'>GamerRater</h1>
+      <h1 className='text-4xl'>Welcome to GamerRater</h1>
     </main>
   )
 }
