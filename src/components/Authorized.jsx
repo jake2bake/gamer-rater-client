5e64443db1378681e0419de1731d5693b31d42c0
Jake Littleton <jakelittleton30@gmail.com>
2026-07-18 11:02:01 -0500

    Authorized file cleaned up

--- src/components/Authorized.jsx
+++ src/components/Authorized.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { NavBar } from "./Navbar.jsx"
 
+
 export const Authorized = () => {
   if (localStorage.getItem("rater_token")) {
     return <>
       <NavBar />
-      <main className="p-4">
+      <main style={{ paddingTop: "60px", paddingLeft: "1rem", paddingRight: "1rem" }}>
         <Outlet />
       </main>
     </>
