e1c2ce41e2ebe8504c1a0c3163cab8f56b38e511
Jake Littleton <jakelittleton30@gmail.com>
2026-07-18 11:03:53 -0500

    Navbar adjusted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,17 +9,17 @@ export const NavBar = () => {
                 <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/allgames"}>All Games</NavLink>
             </li>
             <li className="navbar__item">
-                <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/create"}>Collect a Rock</NavLink>
+                <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/addgame"}>Add a Game</NavLink>
             </li>
-            <li className="navbar__item">
+            {/* <li className="navbar__item">
                 <NavLink className="text-left underline text-blue-600 hover:text-purple-700" to={"/mine"}>My Rocks</NavLink>
-            </li>
+            </li> */}
             {
                 (localStorage.getItem("rater_token") !== null) ?
                     <li className="navbar__item">
                         <button className="underline text-blue-600 hover:text-purple-700"
                             onClick={() => {
-                                localStorage.removeItem("rock_token")
+                                localStorage.removeItem("rater_token")
                                 navigate('/login')
                             }}
                         >Logout</button>
